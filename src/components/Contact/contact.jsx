b7892d1359f127c08bcef14493ba62929546bf72
mohamed tarek <mohamad1tarek1@gmail.com>
2024-08-21 16:52:58 +0300

    a

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -3,48 +3,49 @@ import myImg from "../../Assets/mo4.PNG";
 import Tilt from "react-parallax-tilt";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
-
-
 import React from "react";
-import {
-  AiFillGithub,
-} from "react-icons/ai";
+import { AiFillGithub } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 function ContactUs() {
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    const formData = new FormData(event.target);
+  const form = useRef();
 
-    formData.append("access_key", "5726bc65-f7bf-44b0-b834-101d0e76240b");
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-    const object = Object.fromEntries(formData);
-    const json = JSON.stringify(object);
-
-    const res = await fetch("https://api.web3forms.com/submit", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: json,
-    }).then((res) => res.json());
-
-    if (res.success) {
-      event.target.reset();
-      console.log("Success", res);
-      toast.success("Message sent successfully", {
-        position: "top-right",
-        autoClose: 1000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "dark",
-      });
-    }
+    emailjs
+      .sendForm("service_i7k0kgq", "template_1h8vond", form.current, {
+        publicKey: "a6G56hRX1GiUAhz3b",
+      })
+      .then(
+        () => {
+          toast.success("Message sent successfully", {
+            position: "top-right",
+            autoClose: 1000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+          });
+        },
+        (error) => {
+          toast.error("something went wrong please try again!", {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+          });
+        }
+      );
   };
+
 
   return (
     <>
@@ -58,16 +59,15 @@ function ContactUs() {
                     Contact <span className="purple"> Me </span>
                   </h1>
                 </Col>
-                <form className="form" onSubmit={onSubmit}>
+                <form className="form" ref={form} onSubmit={sendEmail}>
                   <p className="title">Contact Me</p>
                   <div className="flex">
                     <label>
                       <input
                         required
                         placeholder=""
-                        type="name"
                         className="input"
-                        name="name"
+                        type="text" name="user_name"
                       />
                       <span>User Name</span>
                     </label>
@@ -76,16 +76,15 @@ function ContactUs() {
                       <input
                         required
                         placeholder=""
-                        type="email"
                         className="input"
-                        name="email"
+                        type="email" name="user_email"
                       />
                       <span>Email</span>
                     </label>
                   </div>
                   <label>
                     <textarea
-                      style={{ width: "300px" ,  height: "250px" }}
+                      style={{ width: "300px", height: "250px" }}
                       required
                       placeholder=""
                       type="message"
@@ -101,42 +100,42 @@ function ContactUs() {
                 </form>
               </Col>
 
-                <Col md={3} className="myAvtar">
-                  <Tilt>
-                    <img src={myImg} className="img-fluid" alt="avatar" />
-                  </Tilt>
-                  <Row>
-          <Col md={12} className="home-about-social">
-            <h1>FIND ME ON</h1>
-            <p>
-              Feel free to <span className="purple">connect </span>with me
-            </p>
-            <ul className="home-about-social-links">
-              <li className="social-icons">
-                <a
-                  href="https://github.com/Tar2sh12"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <AiFillGithub />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.linkedin.com/in/mohamed-tarek-1452b0244/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <FaLinkedinIn />
-                </a>
-              </li>
-
-            </ul>
-          </Col>
-        </Row>
-                </Col>
+              <Col md={3} className="myAvtar">
+                <Tilt>
+                  <img src={myImg} className="img-fluid" alt="avatar" />
+                </Tilt>
+                <Row>
+                  <Col md={12} className="home-about-social">
+                    <h1>FIND ME ON</h1>
+                    <p>
+                      Feel free to <span className="purple">connect </span>with
+                      me
+                    </p>
+                    <ul className="home-about-social-links">
+                      <li className="social-icons">
+                        <a
+                          href="https://github.com/Tar2sh12"
+                          target="_blank"
+                          rel="noreferrer"
+                          className="icon-colour  home-social-icons"
+                        >
+                          <AiFillGithub />
+                        </a>
+                      </li>
+                      <li className="social-icons">
+                        <a
+                          href="https://www.linkedin.com/in/mohamed-tarek-1452b0244/"
+                          target="_blank"
+                          rel="noreferrer"
+                          className="icon-colour  home-social-icons"
+                        >
+                          <FaLinkedinIn />
+                        </a>
+                      </li>
+                    </ul>
+                  </Col>
+                </Row>
+              </Col>
             </Row>
           </Container>
         </Container>
@@ -152,6 +151,18 @@ function ContactUs() {
         draggable
         pauseOnHover
         theme="light"
+      />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
       />
     </>
   );
